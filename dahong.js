6486e68f906896a54779ed15f38c658713044412
illuSioN_uestc <413972057@qq.com>
2016-11-17 21:00:02 +0800

    update

--- dahong.js
+++ dahong.js
@@ -1,4 +1,7 @@
-function randomize(){//取1-10随机数，其实是根据时间~~~~(>_<)~~~~  
+var textarea = 'fastpostmessage',
+	submitButton = 'fastpostsubmit';
+
+function randomize(){//取1-10随机数，其实是根据时间~~~~(>_<)~~~~
     var now = new Date();  
     num = (now.getSeconds()) % 10;  
     num ++;  
@@ -9,68 +12,68 @@ function interval(){
 	switch(num){
 		case 1:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='威望';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='威望';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 2:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='继续';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='继续';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 3:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='真棒';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='真棒';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 4:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='哈哈';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='哈哈';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 5:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='再来';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='再来';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 6:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='大红楼';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='大红楼';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 7:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='水水';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='水水';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 8:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='看看';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='看看';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 9:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='不纯表';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='不纯表';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 10:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='吃饭';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='吃饭';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 	}
 }
 
 setInterval(function(){
-	document.getElementById('fastpostmessage').style.color='black';
+	document.getElementById(textarea).style.color='black';
 	interval();
 }, 7700);
--- hongbao.js
+++ hongbao.js
@@ -1,70 +1,73 @@
-function randomize(){//取1-10随机数，其实是根据时间~~~~(>_<)~~~~  
+var textarea = 'fastpostmessage',
+	submitButton = 'fastpostsubmit';
+
+function randomize(){//取1-10随机数，其实是根据时间~~~~(>_<)~~~~
     var now = new Date();  
     num = (now.getSeconds()) % 10;  
     num ++;  
 }  
 function interval(){
 	randomize();
-	var TimeInterval = 8800 + num * 100;
+	var TimeInterval = num * 100;
 	switch(num){
 		case 1:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='威望';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='威望';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 2:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='继续';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='继续';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 3:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='真棒';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='真棒';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 4:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='哈哈';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='哈哈';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 5:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='再来';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='再来';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 6:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='大红楼';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='大红楼';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 7:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='水水';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='水水';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 8:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='看看';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='看看';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 9:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='不纯表';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='不纯表';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 		case 10:
 		setTimeout(function(){
-		document.getElementById('fastpostmessage').value='吃饭';
-		document.getElementById('fastpostsubmit').click();
+		document.getElementById(textarea).value='吃饭';
+		document.getElementById(submitButton).click();
 	}, TimeInterval);
 		break;
 	}
