a6c74833f130620c5f96d662668c4d0be052bf86
illuSioN4ng <413972057@qq.com>
2016-11-27 21:57:27 +0800

    uodate

--- dahong.js
+++ dahong.js
@@ -1,79 +1,83 @@
-var textarea = 'fastpostmessage',
-	submitButton = 'fastpostsubmit';
-
-function randomize(){//取1-10随机数，其实是根据时间~~~~(>_<)~~~~
-    var now = new Date();  
-    num = (now.getSeconds()) % 10;  
-    num ++;  
-}  
-function interval(){
-	randomize();
-	var TimeInterval = num * 300;
-	switch(num){
-		case 1:
-		setTimeout(function(){
-		document.getElementById(textarea).value='威望';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 2:
-		setTimeout(function(){
-		document.getElementById(textarea).value='继续';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 3:
-		setTimeout(function(){
-		document.getElementById(textarea).value='真棒';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 4:
-		setTimeout(function(){
-		document.getElementById(textarea).value='哈哈';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 5:
-		setTimeout(function(){
-		document.getElementById(textarea).value='再来';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 6:
-		setTimeout(function(){
-		document.getElementById(textarea).value='大红楼';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 7:
-		setTimeout(function(){
-		document.getElementById(textarea).value='水水';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 8:
-		setTimeout(function(){
-		document.getElementById(textarea).value='看看';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 9:
-		setTimeout(function(){
-		document.getElementById(textarea).value='不纯表';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
-		case 10:
-		setTimeout(function(){
-		document.getElementById(textarea).value='吃饭';
-		document.getElementById(submitButton).click();
-	}, TimeInterval);
-		break;
+var sendMachine = (function(){
+	var textarea = 'livereplymessage',
+		submitButton = 'livereplysubmit';
+	function interval(){
+		//randomize();
+		//console.log(num);
+		var num = Math.ceil(Math.random()* 10),
+			TimeInterval = num * 300;
+		switch(num){
+			case 1:
+				setTimeout(function(){
+					document.getElementById(textarea).value='汉皇重色思倾国';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 2:
+				setTimeout(function(){
+					document.getElementById(textarea).value='御宇多年求不得';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 3:
+				setTimeout(function(){
+					document.getElementById(textarea).value='杨家有女初长成';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 4:
+				setTimeout(function(){
+					document.getElementById(textarea).value='养在深闺人未识';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 5:
+				setTimeout(function(){
+					document.getElementById(textarea).value='天生丽质难自弃';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 6:
+				setTimeout(function(){
+					document.getElementById(textarea).value='一朝选在君王侧';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 7:
+				setTimeout(function(){
+					document.getElementById(textarea).value='回眸一笑百媚生';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 8:
+				setTimeout(function(){
+					document.getElementById(textarea).value='六宫粉黛无颜色';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 9:
+				setTimeout(function(){
+					document.getElementById(textarea).value='春寒赐浴华清池';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+			case 10:
+				setTimeout(function(){
+					document.getElementById(textarea).value='温泉水滑洗凝脂';
+					document.getElementById(submitButton).click();
+				}, TimeInterval);
+				break;
+		}
 	}
-}
 
-setInterval(function(){
-	document.getElementById(textarea).style.color='black';
-	interval();
-}, 7700);
+	return {
+		init: function(){
+			setInterval(function(){
+				document.getElementById(textarea).style.color='black';
+				interval();
+			}, 4800);
+		}
+	}
+})();
+
+sendMachine.init();
