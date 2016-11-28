66773cf9b972b44a8c404df4d78331cb7fb10300
illuSioN_uestc <413972057@qq.com>
2016-11-28 19:14:13 +0800

    update

--- dahong.js
+++ dahong.js
@@ -1,6 +1,10 @@
 var sendMachine = (function(){
 	var textarea = 'livereplymessage',
-		submitButton = 'livereplysubmit';
+		submitButton = 'livereplysubmit',
+		strArr = [
+			'汉皇重色思倾国', '御宇多年求不得', '杨家有女初长成', '养在深闺人未识', '天生丽质难自弃',
+			'一朝选在君王侧', '回眸一笑百媚生', '六宫粉黛无颜色', '春寒赐浴华清池', '温泉水滑洗凝脂'
+		];
 	function interval(){
 		//randomize();
 		//console.log(num);
@@ -9,61 +13,61 @@ var sendMachine = (function(){
 		switch(num){
 			case 1:
 				setTimeout(function(){
-					document.getElementById(textarea).value='汉皇重色思倾国';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 2:
 				setTimeout(function(){
-					document.getElementById(textarea).value='御宇多年求不得';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 3:
 				setTimeout(function(){
-					document.getElementById(textarea).value='杨家有女初长成';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 4:
 				setTimeout(function(){
-					document.getElementById(textarea).value='养在深闺人未识';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 5:
 				setTimeout(function(){
-					document.getElementById(textarea).value='天生丽质难自弃';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 6:
 				setTimeout(function(){
-					document.getElementById(textarea).value='一朝选在君王侧';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 7:
 				setTimeout(function(){
-					document.getElementById(textarea).value='回眸一笑百媚生';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 8:
 				setTimeout(function(){
-					document.getElementById(textarea).value='六宫粉黛无颜色';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 9:
 				setTimeout(function(){
-					document.getElementById(textarea).value='春寒赐浴华清池';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
 			case 10:
 				setTimeout(function(){
-					document.getElementById(textarea).value='温泉水滑洗凝脂';
+					document.getElementById(textarea).value = strArr[num - 1];
 					document.getElementById(submitButton).click();
 				}, TimeInterval);
 				break;
